6d2bbfc3ddbdaeaf53c0caf74a71a6e3fd2918dd
DevBash1 <ikorosamuel1@gmail.com>
2021-10-03 00:56:28 +0100

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   download_url = 'https://github.com/DevBash1/Qaver/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['PHP', 'webserver', 'server', 'http'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'urllib',
+          
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
