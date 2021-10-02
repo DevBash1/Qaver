7643d44698296b9b4746043ea296adeaf55d10be
DevBash1 <ikorosamuel1@gmail.com>
2021-10-03 00:36:39 +0100

    new files

--- index.py
+++
@@ -1,7 +0,0 @@
-from qaver import Server,Accepts
-
-accepts = Accepts();
-accepts.reset([".html",".js"])
-
-server = Server()
-server.start("localhost",8080)
--- server.py
+++
@@ -1,6 +0,0 @@
-import os
-
-file = _FILES("file1")
-if(file):
-    os.rename(file,"song.mp3")
-    print("song.mp3")
