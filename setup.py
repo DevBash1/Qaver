6afe7b524de93ddb491130ff4f8a7d2eead1c2af
DevBash1 <ikorosamuel1@gmail.com>
2021-10-03 00:43:27 +0100

    new files

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Dev Bash',                   # Type in your name
   author_email = 'ikorosamuel1@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/DevBash1/Qaver',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/DevBash1/Qaver/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['PHP', 'webserver', 'server', 'http'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'urllib',
