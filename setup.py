b6818a2522153d11cf52fb7e4ff4b7721561a884
DevBash1 <ikorosamuel1@gmail.com>
2021-10-03 01:12:40 +0100

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
 from distutils.core import setup
 setup(
-  name = 'Qaver',         # How you named your package folder (MyLib)
-  packages = ['Qaver'],   # Chose the same as "name"
+  name = 'PyQaver',         # How you named your package folder (MyLib)
+  packages = ['PyQaver'],   # Chose the same as "name"
   version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A PHP like WebServer for Python',   # Give a short description about your library
