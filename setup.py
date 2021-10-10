8143ae708f9696a956bd2db483947e20e68418a9
Dev Bash <55766346+DevBash1@users.noreply.github.com>
2021-10-10 03:29:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'PyQaver',         # How you named your package folder (MyLib)
   packages = ['PyQaver'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A PHP like WebServer for Python',   # Give a short description about your library
   author = 'Dev Bash',                   # Type in your name
   author_email = 'ikorosamuel1@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/DevBash1/Qaver',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/DevBash1/Qaver/archive/refs/tags/v_03.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/DevBash1/Qaver/archive/refs/tags/v_05.tar.gz',    # I explain this later on
   keywords = ['PHP', 'webserver', 'server', 'http'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           
